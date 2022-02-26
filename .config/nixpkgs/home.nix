2070a6418a005a4ccc68686b7eaf1449e9d60d34
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2022-02-26 22:22:17 +0000

    git setup

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -1,9 +1,19 @@
 { pkgs, ...}: {
   home.packages = [ 
-    pkgs.git 
     pkgs.zsh 
     pkgs.alacritty 
     pkgs.firefox 
     pkgs.brave] ;
+
+  programs.git = {
+    enable = true;
+    userName = "Matt Rixman";
+    userEmail = "MatrixManAtYrService@users.noreply.github.com";
+    extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+    };
+  };
 }
 
